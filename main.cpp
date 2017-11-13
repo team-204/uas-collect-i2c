938a786e61be53d7cc02a1299ef64c9399a26fe8
Andrew Halaney <ajhalaney@gmail.com>
2017-11-12 20:48:36 -0600

    Add constructor for mpl3115a2 (#3)
    Added a constructor for mpl3115a2 that attempts to open the i2c adapter
    given and confirm via and i2c read that the device located at the
    address is a mpl3115a2.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 #include "mpl3115a2.hpp"
 
 
-int main(void)
+int main(int argc, char **argv)
 {
     std::cout << "Hello world!" << std::endl;
-    MPL3115A2 temp;
-    temp.doNothing();
+    if (argc != 2)
+    {
+        std::cerr << "Please give the i2c adapter number (found using i2cdetect -l)" << std::endl;
+        return 1;
+    }
+    std::string adapter(argv[1]);
+    MPL3115A2 temp(stoi(adapter));
     return 0;
 }
--- mpl3115a2.hpp
+++ mpl3115a2.hpp
@@ -1,9 +1,17 @@
 #ifndef MPL3115A2_HPP
 #define MPL3115A2_HPP
+
+#include <string>
+
+
 class MPL3115A2
 {
     public:
-        void doNothing(void);
+        // Attempts to open the i2c connection at the adapterNumber
+        MPL3115A2(unsigned int adapterNumber);
     private:
+        std::string m_i2cFilename;
+        int m_i2cFile;
 };
+
 #endif
