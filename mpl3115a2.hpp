e336de8687cfd599ed92a6db2c825678147b5ad4
Andrew Halaney <ajhalaney@gmail.com>
2017-11-13 19:43:18 -0600

    Fix WHO_AM_I read & refactor i2c calls
    Refactored i2c read and write calls to their own functions. Definitely
    still naively using the i2c-dev interface. There seems to be a more
    ideal way to read and write by using an ioctl call. There are not many
    examples online though and the kernel documentation is unclear on how
    this should be used.

--- mpl3115a2.hpp
+++ mpl3115a2.hpp
@@ -1,15 +1,21 @@
 #ifndef MPL3115A2_HPP
 #define MPL3115A2_HPP
 
+#include <stdint.h>
 #include <string>
+#include <vector>
 
 
 class MPL3115A2
 {
     public:
         // Attempts to open the i2c connection at the adapterNumber
-        MPL3115A2(unsigned int adapterNumber);
+        MPL3115A2(const unsigned int adapterNumber);
     private:
+        std::vector<uint8_t> readBytes(const uint8_t reg, const unsigned int size) const;
+
+        // Expects the first element in data to be the register to be written to
+        void writeBytes(const std::vector<uint8_t> &data) const;
         std::string m_i2cFilename;
         int m_i2cFile;
 };
