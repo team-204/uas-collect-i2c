7cc2c3f97d4132f0eee64534fb4915ce9933e098
Andrew Halaney <ajhalaney@gmail.com>
2017-11-19 21:47:37 -0600

    Fix i2c calls to use ioctl mixed transaction
    Previously, basic calls to write and read file descriptors were used.
    Now the ioctl call is used which allows for a write and read to occur in
    one transaction. This fixes the bug where you cannot read from a
    selected register on a device.
    Also added the datasheet and configuration manual for the MPL3115A2. This way
    offline work can be done easier.

--- mpl3115a2.hpp
+++ mpl3115a2.hpp
@@ -12,10 +12,8 @@ class MPL3115A2
         // Attempts to open the i2c connection at the adapterNumber
         MPL3115A2(const unsigned int adapterNumber);
     private:
-        std::vector<uint8_t> readBytes(const uint8_t reg, const unsigned int size) const;
-
-        // Expects the first element in data to be the register to be written to
-        void writeBytes(const std::vector<uint8_t> &data) const;
+        uint8_t readByte(uint8_t reg) const;
+        void writeByte(uint8_t reg, uint8_t data) const;
         std::string m_i2cFilename;
         int m_i2cFile;
 };
